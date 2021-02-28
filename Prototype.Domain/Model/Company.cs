8d9f367f2b02e22f2f202bd86bd3ba0c8d335fe7
=anis1011 <=maharzn11@gmail.com>
2021-02-28 22:34:14 +0545

    domain model added

---
+++ Prototype.Domain/Model/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Prototype.Domain.Model
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
