a167fb1f1f8ca08cc2af5ad14b4d20536f5ccead
=anis1011 <=maharzn11@gmail.com>
2021-02-28 22:38:12 +0545

    DTO added

---
+++ Prototype.Domain/DTO/Authentication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype.Domain.DTO
+{
+    public class SigninDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class SignupDTO : SigninDTO
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class SigninResponseDTO
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string ExpireIn { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Prototype.Domain/DTO/User.cs
@@ -0,0 +1,25 @@
+﻿using Prototype.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype.Domain.DTO
+{
+    public class UserDTO : SignupDTO
+    {
+        public Guid? Id { get; set; }
+        public List<string> Roles { get; set; }
+
+        public UserDTO MapToUserDTO(ApplicationUser user)
+        {
+            return new UserDTO()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                MiddleName = user.MiddleName,
+                LastName = user.LastName
+            };
+        }
+    }
+}
